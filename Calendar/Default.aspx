16cdbe654f1b2103ebc483a001605f40c33d92bb
amy61789 <amy61789@outlook.com>
2017-01-17 20:22:20 -0800

    Assignment 2
    Calendar Form

---
+++ Calendar/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1 style = "color:PowderBlue">2017 Event Calendar</h1>
+        <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged">
+            <SelectedDayStyle BackColor="PowderBlue"></SelectedDayStyle>
+            <DayStyle BackColor="#FF8080" BorderWidth="2" BorderStyle="Groove"></DayStyle>
+        </asp:Calendar>
+        <asp:BulletedList ID="EventList" runat="server"></asp:BulletedList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Calendar/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string[,] events = new string[6, 2];
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        events[0, 0] = "Oil Change";
+        events[0, 1] = "1/10/2017";
+        events[1, 0] = "Dentist Appointment";
+        events[1, 1] = "1/25/2017";
+        events[2, 0] = "Dinner Plans with Steve";
+        events[2, 1] = "1/17/2017";
+        events[3, 0] = "Calendar Project Due";
+        events[3, 1] = "1/18/2017";
+        events[4, 0] = "Temperature Conversion Project Due";
+        events[4, 1] = "1/17/2017";
+        events[5, 0] = "Wine Tasting";
+        events[5, 1] = "1/31/2017";
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        EventList.Items.Clear();
+        GetEvents();
+    }
+    protected void GetEvents()
+    {
+            if (Calendar1.SelectedDate == DateTime.Parse(events[0, 1]))
+            {
+                EventList.Items.Add(events[0, 0]);
+                EventList.ForeColor = System.Drawing.Color.Green;
+            }
+            if (Calendar1.SelectedDate == DateTime.Parse(events[1, 1]))
+            {
+                EventList.Items.Add(events[1, 0]);
+                EventList.ForeColor = System.Drawing.Color.Red;
+            }
+            if (Calendar1.SelectedDate == DateTime.Parse(events[2, 1]))
+            {
+                EventList.Items.Add(events[2, 0]);
+                EventList.ForeColor = System.Drawing.Color.Pink;
+            }
+            if (Calendar1.SelectedDate == DateTime.Parse(events[3, 1]))
+            {
+                EventList.Items.Add(events[3, 0]);
+                EventList.ForeColor = System.Drawing.Color.Purple;
+            }
+            if (Calendar1.SelectedDate == DateTime.Parse(events[4, 1]))
+            {
+                EventList.Items.Add(events[4, 0]);
+            }
+            if (Calendar1.SelectedDate == DateTime.Parse(events[5, 1]))
+            {
+                EventList.Items.Add(events[5, 0]);
+                EventList.ForeColor = System.Drawing.Color.Turquoise;
+            }
+            if (EventList.Items.Count == 0)
+            {
+                EventList.Items.Add("No events for today.");
+                EventList.ForeColor = System.Drawing.Color.PowderBlue;
+            }
+    }
+}
